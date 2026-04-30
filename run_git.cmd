90f1d2b4174f8f8adc9330c6be88a77057348008
shanmukhi406 <shanmukhichinni9@gmail.com>
2026-04-30 17:58:18 +0530

    first commit

---
+++ run_git.cmd
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote remove origin
+git remote add origin https://github.com/shanmukhi406/shopsphere.git
+git push -u origin main
